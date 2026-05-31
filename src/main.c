ec90b636a6d59e7954f3a205645b3653d54d1475
ParadoxicXenos <mehroznajif88@gmail.com>
2026-05-31 21:52:50 +1200

    file compat work

--- src/main.c
+++ src/main.c
@@ -2,14 +2,19 @@
 #include "include/lexer.h"
 #include "include/parser.h"
 #include "include/visitor.h"
+#include "include/io.h"
+void print_help(){
+
+    printf("Usage:\n paradox.out <filename>\n");
+    exit(1);
+}
 
 int main(int argc, char* argv[])
 {
-    lexer_T* lexer = init_lexer(
-        "var x = \"Hello\";\n"
-        "var y = \"World\";\n"
-        "print(x, y);\n"
-    );
+    if (argc < 2)
+        print_help();
+    lexer_T* lexer = init_lexer(get_file_contents(argv[1]));
+            
 
     parser_T* parser = init_parser(lexer);
     AST_T* root = parser_parse(parser);
